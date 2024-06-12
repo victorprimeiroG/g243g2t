5f0cd2151d4ae846a2baad5dad45635a8f55f5d1
victorprimeiroG <110112719+victorprimeiroG@users.noreply.github.com>
2024-06-12 08:10:32 -0300

    Add files via upload

---
+++ aula1/script/script.js
@@ -0,0 +1,28 @@
+const btn = document.querySelector("#btnLogin");
+const inputUsuario = document.querySelector("#usuario");
+const inputSenha = document.querySelector("#senha");
+
+(()=>{
+    let usuario = localStorage.getItem("usuario");
+    if(usuario){
+        window.location.href = "http://www.uol.com.br";
+    }
+})();
+
+btn.onclick = (e) =>{
+    e.preentDefault();
+    let usuario = inputUsuario.value;
+    let senha = inputSenha.value;
+    if(!usuario && !senha){
+        inputUsuario.focus();
+        return;
+    }
+    if(usuario === "jesus"){
+        if(senha === "123"){
+            localStorage.setItem("usuario",usuario);
+            window.location.href = "http://www.uol.com.br";
+        }else{
+            inputUsuario.focus();
+        }
+    }
+}
